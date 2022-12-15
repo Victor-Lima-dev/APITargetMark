4a1395b411f2390fddabaf28d9f67af4aacf1a89
VictorLima <victoraugustolima1@gmail.com>
2022-12-15 16:19:20 -0300

    Criar classe Relatorio

---
+++ APITargetMark/Models/Relatorio.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APITargetMark.Models
+{
+    public class Relatorio
+    {
+        public int RelatorioId { get; set; }
+        //obrigatorio e limite de caracteres
+        [Required]
+        [StringLength(15, MinimumLength = 5)]
+        public string Titulo { get; set; }
+        public int MensagensVisualizadas { get; set; }
+        public int MensagensInteragidas { get; set; }
+        public int TaxaConversao { get; set; }
+        public int CampanhaId { get; set; }
+        
+    }
+}
