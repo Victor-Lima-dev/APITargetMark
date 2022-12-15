ca2bb26e382fd5fde9670fd6276590d861f0f401
VictorLima <victoraugustolima1@gmail.com>
2022-12-15 16:25:32 -0300

    Criar classe de contexto mapear todas as classes

---
+++ APITargetMark/Context/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using APITargetMark.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APITargetMark.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Mensagem> Mensagens { get; set; }
+        public DbSet<Empresa> Empresas { get; set; }
+        public DbSet<Campanha> Campanhas { get; set; }
+        public DbSet<Relatorio> Relatorios { get; set; }
+
+
+
+
+
+
+
+    }
+}
