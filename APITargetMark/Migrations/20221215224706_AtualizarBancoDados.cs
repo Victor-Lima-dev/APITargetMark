bebbdc09a1f0eb6ae21a6dd3f62971c33fa3316c
VictorLima <victoraugustolima1@gmail.com>
2022-12-15 19:48:36 -0300

    banco de dados atualizado houve uma mudança no model da classe empresa

---
+++ APITargetMark/Migrations/20221215224706_AtualizarBancoDados.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APITargetMark.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizarBancoDados : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EmpresaId",
+                table: "Relatorios",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmpresaId",
+                table: "Relatorios");
+        }
+    }
+}
