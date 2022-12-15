7363f88e76a4b4070deae37abcfc19256a66812b
VictorLima <victoraugustolima1@gmail.com>
2022-12-15 16:16:02 -0300

    Criar classe Mensagem

---
+++ APITargetMark/Models/Mensagem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APITargetMark.Models
+{
+    public class Mensagem
+    {
+        public int MensagemId { get; set; }
+        public string Titulo { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 5)]
+        public string Texto { get; set; }
+        public int CampanhaId { get; set; }
+    }
+}
