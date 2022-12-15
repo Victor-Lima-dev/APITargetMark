33b30f82a4839b699c46250eb743cd4ae16a3f9f
VictorLima <victoraugustolima1@gmail.com>
2022-12-15 16:02:10 -0300

    Adicionar ao código a classe Cliente DataAnnotations utilizadas

---
+++ APITargetMark/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APITargetMark.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        [Required]
+        //limitar caracteres
+        [StringLength(15, MinimumLength = 5)]
+        public string Nome { get; set; }
+        //limitar valor, maximo 100
+        [Range(0, 100)]
+        public int Idade { get; set; }
+        [Required]
+        [StringLength(15, MinimumLength = 5)]
+        public string Genero { get; set; }
+        [Required]
+        //limitar caracteres, minimo 6 maximo 12
+        [StringLength(12, MinimumLength = 2)]
+        public string Regiao { get; set; }
+    }
+}
