4767000fa2a40a8d5298ed875c8cab9130ce515d
VictorLima <victoraugustolima1@gmail.com>
2022-12-15 16:04:12 -0300

    Adicionar classe Empresa

---
+++ APITargetMark/Models/Empresa.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace APITargetMark.Models;
+
+
+public class Empresa
+{
+    public int EmpresaId { get; set; }
+    [Required]
+    [StringLength(15, MinimumLength = 3)]
+    public string Nome { get; set; }
+}
